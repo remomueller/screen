b52a9f1e500f1889c414556d63f586a26f304977
Remo Mueller <remosm@gmail.com>
2012-07-17 15:54:52 -0400

    Use Rails default <tt>config.active_record.whitelist_attributes = true</tt> and <tt>config.active_record.mass_assignment_sanitizer = :strict</tt>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,20 @@ class ApplicationController < ActionController::Base
 
   layout "contour/layouts/application"
 
+  protected
+
+  def check_system_admin
+    redirect_to root_path, alert: "You do not have sufficient privileges to access that page." unless current_user.system_admin?
+  end
+
+  def check_screener
+    redirect_to root_path, alert: "You do not have sufficient privileges to access that page." unless current_user.screener?
+  end
+
+  def check_screener_or_subject_handler
+    redirect_to root_path, alert: "You do not have sufficient privileges to access that page." unless current_user.screener? or current_user.subject_handler?
+  end
+
   def pretty_phone(phone)
     digits = phone.to_s.gsub(/[^\d]/, '')
     if digits.size == 10
@@ -19,19 +33,12 @@ class ApplicationController < ActionController::Base
     date_string.to_s.split('/').last.size == 2 ? Date.strptime(date_string, "%m/%d/%y") : Date.strptime(date_string, "%m/%d/%Y") rescue default_date
   end
 
-
-  protected
-
-  def check_system_admin
-    redirect_to root_path, alert: "You do not have sufficient privileges to access that page." unless current_user.system_admin?
-  end
-
-  def check_screener
-    redirect_to root_path, alert: "You do not have sufficient privileges to access that page." unless current_user.screener?
-  end
-
-  def check_screener_or_subject_handler
-    redirect_to root_path, alert: "You do not have sufficient privileges to access that page." unless current_user.screener? or current_user.subject_handler?
+  def scrub_order(model, params_order, default_order)
+    (params_column, params_direction) = params_order.to_s.strip.downcase.split(' ')
+    direction = (params_direction == 'desc' ? 'DESC' : nil)
+    column_name = (model.column_names.collect{|c| model.table_name + "." + c}.select{|c| c == params_column}.first)
+    order = column_name.blank? ? default_order : [column_name, direction].compact.join(' ')
+    order
   end
 
   private
