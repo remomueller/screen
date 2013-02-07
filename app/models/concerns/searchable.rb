d252a75b258db760b8ff32a612afac4c5a9f99bc
Remo Mueller <remosm@gmail.com>
2013-02-07 09:21:13 -0500

    Added Deletable and Searchable Concerns

---
+++ app/models/concerns/searchable.rb
@@ -0,0 +1,9 @@
+module Searchable
+  extend ActiveSupport::Concern
+
+  included do
+    scope :search, lambda { |arg| where('LOWER(name) LIKE ? or LOWER(description) LIKE ?', arg.to_s.downcase.gsub(/^| |$/, '%'), arg.to_s.downcase.gsub(/^| |$/, '%')) }
+    scope :search_name, lambda { |arg| where('LOWER(name) LIKE ?', arg.to_s.downcase.gsub(/^| |$/, '%')) }
+  end
+
+end
