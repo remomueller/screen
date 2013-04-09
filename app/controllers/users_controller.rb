0f1b824c1970d2700741868ec034f9e31cf94ad0
Remo Mueller <remosm@gmail.com>
2013-04-09 16:10:37 -0400

    Updated Users with Rails 4 strong_parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,28 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :check_system_admin, only: [:new, :create, :edit, :update, :destroy]
+  before_action :check_system_admin, only: [ :new, :create, :edit, :update, :destroy ]
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+  before_action :redirect_without_user, only: [ :show, :edit, :update, :destroy ]
 
   def index
     unless current_user.system_admin? or params[:format] == 'json'
       redirect_to root_path, alert: "You do not have sufficient privileges to access that page."
       return
     end
-    # current_user.update_column :users_per_page, params[:users_per_page].to_i if params[:users_per_page].to_i >= 10 and params[:users_per_page].to_i <= 200
-    user_scope = User.current
-    @search_terms = (params[:search] || params[:q]).to_s.gsub(/[^0-9a-zA-Z]/, ' ').split(' ')
-    @search_terms.each{|search_term| user_scope = user_scope.search(search_term) }
 
     @order = scrub_order(User, params[:order], 'users.current_sign_in_at DESC')
-    user_scope = user_scope.order(@order)
+    @users = User.current.search(params[:search] || params[:q]).order(@order).page(params[:page]).per( 20 )
 
-    @users = user_scope.page(params[:page]).per(20) # (current_user.users_per_page)
     respond_to do |format|
       format.html
       format.js
-      format.json { render json: params[:q].to_s.split(',').collect{|u| (u.strip.downcase == 'me') ? {name: current_user.name, id: current_user.name} : {name: u.strip.titleize, id: u.strip.titleize}} + @users.collect{|u| {name: u.name, id: u.name}}}
+      format.json do # TODO: Put into jbuilder instead!
+        render json: params[:q].to_s.split(',').collect{ |u| (u.strip.downcase == 'me') ? { name: current_user.name, id: current_user.name } : { name: u.strip.titleize, id: u.strip.titleize } } + @users.collect{ |u| { name: u.name, id: u.name } }
+      end
     end
   end
 
   def show
-    @user = User.current.find_by_id(params[:id])
-    redirect_to users_path unless @user
   end
 
   # def new
@@ -33,8 +30,6 @@ class UsersController < ApplicationController
   # end
 
   def edit
-    @user = User.current.find_by_id(params[:id])
-    redirect_to users_path unless @user
   end
 
   # # This is in registrations_controller.rb
@@ -42,37 +37,36 @@ class UsersController < ApplicationController
   # end
 
   def update
-    @user = User.current.find_by_id(params[:id])
-    if @user and @user.update_attributes(post_params)
-      original_status = @user.status
-      @user.update_column :system_admin, params[:user][:system_admin]
-      @user.update_column :screener, params[:user][:screener]
-      @user.update_column :subject_handler, params[:user][:subject_handler]
-      @user.update_column :access_phi, params[:user][:access_phi]
-      @user.update_column :status, params[:user][:status]
+    original_status = @user.status
+    if @user.update(user_params)
       UserMailer.status_activated(@user).deliver if Rails.env.production? and original_status != @user.status and @user.status == 'active'
-      redirect_to(@user, notice: 'User was successfully updated.')
-    elsif @user
-      render action: "edit"
+      redirect_to @user, notice: 'User was successfully updated.'
     else
-      redirect_to users_path
+      render action: 'edit'
     end
   end
 
   def destroy
-    @user = User.find_by_id(params[:id])
-    @user.destroy if @user
+    @user.destroy
     redirect_to users_path, notice: 'User was successfully deleted.'
   end
 
   private
 
-  def post_params
-    params[:user] ||= {}
+    def set_user
+      @user = User.current.find_by_id(params[:id])
+    end
 
-    params[:user].slice(
-      :first_name, :last_name, :email
-    )
-  end
+    def redirect_without_user
+      empty_response_or_root_path(users_path) unless @user
+    end
+
+    def user_params
+      params[:user] ||= {}
+
+      params.require(:user).permit(
+        :first_name, :last_name, :email, :password, :password_confirmation, :system_admin, :screener, :subject_handler, :access_phi, :status
+      )
+    end
 
 end
