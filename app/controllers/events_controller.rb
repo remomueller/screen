04693380261d48d97367a9745fe98afc7ec29374
Remo Mueller <remosm@gmail.com>
2013-04-10 12:13:05 -0400

    Updated Events with Rails 4 strong_parameters

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,14 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
   before_action :check_screener_or_subject_handler
+  before_action :set_event, only: [ :show, :edit, :update, :destroy ]
+  before_action :redirect_without_event, only: [ :show, :edit, :update, :destroy ]
 
+  # GET /events
+  # GET /events.json
   def index
-    # current_user.update_column :events_per_page, params[:events_per_page].to_i if params[:events_per_page].to_i >= 10 and params[:events_per_page].to_i <= 200
-    event_scope = Event.current # current_user.all_viewable_events
+    @order = scrub_order(Event, params[:order], 'events.event_time DESC')
+    event_scope = Event.current.order(@order)
 
     if params[:mrn].to_s.split(',').size > 1
       event_scope = event_scope.with_subject_code(params[:mrn].to_s.gsub(/\s/, '').split(','))
@@ -17,60 +21,78 @@ class EventsController < ApplicationController
     event_scope = event_scope.with_patient(params[:patient_id]) unless params[:patient_id].blank?
     event_scope = event_scope.subject_code_not_blank unless current_user.screener?
 
-    @order = scrub_order(Event, params[:order], 'events.event_time DESC')
-    event_scope = event_scope.order(@order)
-
     @events = event_scope.page(params[:page]).per(20) # (current_user.events_per_page)
   end
 
-
+  # GET /events/1
+  # GET /events/1.json
   def show
-    @event = Event.find(params[:id])
   end
 
+  # GET /events/new
   def new
     @event = Event.new
   end
 
+  # GET /events/1/edit
   def edit
-    @event = Event.find(params[:id])
   end
 
+  # POST /events
+  # POST /events.json
   def create
-    @event = Event.new(post_params)
+    @event = Event.new(event_params)
 
-    if @event.save
-      redirect_to @event, notice: 'Event was successfully created.'
-    else
-      render action: "new"
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @event }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
     end
   end
 
+  # PUT /events/1
+  # PUT /events/1.json
   def update
-    @event = Event.find(params[:id])
-
-    if @event.update_attributes(post_params)
-      redirect_to @event, notice: 'Event was successfully updated.'
-    else
-      render action: "edit"
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
     end
   end
 
+  # DELETE /events/1
+  # DELETE /events/1.json
   def destroy
-    @event = Event.find(params[:id])
     @event.destroy
 
-    redirect_to events_path, notice: 'Event was successfully deleted.'
+    respond_to do |format|
+      format.html { redirect_to events_path, notice: 'Event was successfully deleted.' }
+      format.json { head :no_content }
+    end
   end
 
   private
 
-  def post_params
-    params[:event] ||= {}
+    def set_event
+      @event = Event.find_by_id(params[:id])
+    end
 
-    params[:event].slice(
-      :patient_id, :class_name, :class_id, :event_time, :name
-    )
-  end
+    def redirect_without_event
+      empty_response_or_root_path unless @event
+    end
+
+    def event_params
+      params.require(:event).permit(
+        :patient_id, :class_name, :class_id, :event_time, :name
+      )
+    end
 
 end
