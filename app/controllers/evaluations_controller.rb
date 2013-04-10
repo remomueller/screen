9d52b978208a4e312a066928616690e11d75bb41
Remo Mueller <remosm@gmail.com>
2013-04-10 12:19:53 -0400

    Updated Evaluations with Rails 4 strong_parameters

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,10 +1,14 @@
 class EvaluationsController < ApplicationController
   before_action :authenticate_user!
   before_action :check_screener_or_subject_handler
+  before_action :set_evaluation, only: [ :show, :edit, :update, :destroy ]
+  before_action :redirect_without_evaluation, only: [ :show, :edit, :update, :destroy ]
 
+  # GET /evaluations
+  # GET /evaluations.json
   def index
-    # current_user.update_column :evaluations_per_page, params[:evaluations_per_page].to_i if params[:evaluations_per_page].to_i >= 10 and params[:evaluations_per_page].to_i <= 200
-    evaluation_scope = Evaluation.current # current_user.all_viewable_evaluations
+    @order = scrub_order(Evaluation, params[:order], 'evaluations.patient_id')
+    evaluation_scope = Evaluation.current.order(@order)
 
     if params[:mrn].to_s.split(',').size > 1
       evaluation_scope = evaluation_scope.with_subject_code(params[:mrn].to_s.gsub(/\s/, '').split(','))
@@ -35,105 +39,121 @@ class EvaluationsController < ApplicationController
     evaluation_scope = evaluation_scope.where( administration_type: params[:administration_type] ) unless params[:administration_type].blank?
     evaluation_scope = evaluation_scope.where( evaluation_type: params[:evaluation_type] ) unless params[:evaluation_type].blank?
 
-    @order = scrub_order(Evaluation, params[:order], 'evaluations.patient_id')
-    evaluation_scope = evaluation_scope.order(@order)
-
     if params[:format] == 'csv'
       generate_csv(evaluation_scope)
       return
     end
 
-    @evaluation_count = evaluation_scope.count
-    @evaluations = evaluation_scope.page(params[:page]).per(20) # (current_user.evaluations_per_page)
+    @evaluations = evaluation_scope.page(params[:page]).per(20)
   end
 
+  # GET /evaluations/1
+  # GET /evaluations/1.json
   def show
     @evaluation = Evaluation.find_by_id(params[:id])
     redirect_to root_path unless @evaluation and @evaluation.patient.editable_by?(current_user)
   end
 
+  # GET /evaluations/new?patient_id=1
   def new
     @evaluation = Evaluation.new(patient_id: params[:patient_id])
     redirect_to root_path unless @evaluation and @evaluation.patient.editable_by?(current_user)
   end
 
+  # GET /evaluations/1/edit
   def edit
     @evaluation = Evaluation.find_by_id(params[:id])
     redirect_to root_path unless @evaluation and @evaluation.patient.editable_by?(current_user)
   end
 
+  # POST /evaluations
+  # POST /evaluations.json
   def create
-    @evaluation = current_user.evaluations.new(post_params)
+    @evaluation = current_user.evaluations.new(evaluation_params)
 
-    if @evaluation.save
-      redirect_to @evaluation.patient, notice: 'Evaluation was successfully created.'
-    else
-      render action: "new"
-    end
-  end
-
-  def update
-    @evaluation = Evaluation.find_by_id(params[:id])
-
-    if @evaluation and @evaluation.patient.editable_by?(current_user)
-      if @evaluation.update_attributes(post_params)
-        redirect_to @evaluation, notice: 'Evaluation was successfully updated.'
+    respond_to do |format|
+      if @evaluation.save
+        format.html { redirect_to @evaluation.patient, notice: 'Evaluation was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @evaluation }
       else
-        render action: "edit"
+        format.html { render action: 'new' }
+        format.json { render json: @evaluation.errors, status: :unprocessable_entity }
       end
-    else
-      redirect_to root_path
     end
   end
 
+  # PUT /evaluations/1
+  # PUT /evaluations/1.json
+  def update
+    respond_to do |format|
+      if @evaluation.update(evaluation_params)
+        format.html { redirect_to @evaluation, notice: 'Evaluation was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @evaluation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /evaluations/1
+  # DELETE /evaluations/1.json
   def destroy
-    @evaluation = Evaluation.find_by_id(params[:id])
-    if @evaluation and @evaluation.patient.editable_by?(current_user)
-      @evaluation.destroy
-      redirect_to evaluations_path, notice: 'Evaluation was successfully deleted.'
-    else
-      redirect_to root_path
+    @evaluation.destroy
+
+    respond_to do |format|
+      format.html { redirect_to evaluations_path, notice: 'Evaluation was successfully deleted.' }
+      format.json { head :no_content }
     end
   end
 
   private
 
-  def generate_csv(evaluation_scope)
-    @csv_string = CSV.generate do |csv|
-      csv << ["Patient ID", "Subject Code", "Administration Type", "Evaluation Type", "Source", "Administration Date", "Receipt Date", "Scored Date", "AHI", "Eligibility", "Exclusion", "Exclusion Code", "Status", "Comments"]
-      evaluation_scope.each do |evaluation|
-        csv << [
-          evaluation.patient.id,
-          evaluation.patient.subject_code,
-          evaluation.administration_type_name,
-          evaluation.evaluation_type_name,
-          evaluation.source,
-          evaluation.administration_date.strftime("%Y-%m-%d"),
-          evaluation.receipt_date ? evaluation.receipt_date.strftime("%Y-%m-%d") : '',
-          evaluation.scored_date ? evaluation.scored_date.strftime("%Y-%m-%d") : '',
-          evaluation.ahi,
-          evaluation.eligibility,
-          evaluation.exclusion_name,
-          evaluation.exclusion,
-          evaluation.status,
-          evaluation.comments
-        ]
+    def set_evaluation
+      evaluation = Evaluation.find_by_id(params[:id])
+      @evaluation = evaluation if evaluation and evaluation.patient.editable_by?(current_user)
+    end
+
+    def redirect_without_evaluation
+      empty_response_or_root_path unless @evaluation
+    end
+
+    def evaluation_params
+      params[:evaluation] ||= {}
+
+      [:administration_date, :expected_receipt_date, :receipt_date, :storage_date, :reimbursement_form_date, :scored_date].each do |date|
+        params[:evaluation][date] = parse_date(params[:evaluation][date])
       end
-    end
-    send_data @csv_string, type: 'text/csv; charset=iso-8859-1; header=present',
-                          disposition: "attachment; filename=\"Evaluations #{Time.now.strftime("%Y.%m.%d %Ih%M %p")}.csv\""
-  end
 
-  def post_params
-    params[:evaluation] ||= {}
-
-    [:administration_date, :expected_receipt_date, :receipt_date, :storage_date, :reimbursement_form_date, :scored_date].each do |date|
-      params[:evaluation][date] = parse_date(params[:evaluation][date])
+      params.require(:evaluation).permit(
+        :patient_id, :administration_type, :evaluation_type, :administration_date, :source, :embletta_unit_number, :expected_receipt_date, :receipt_date, :storage_date, :subject_notified, :reimbursement_form_date, :scored_date, :ahi, :eligibility, :exclusion, :status, :comments
+      )
     end
 
-    params[:evaluation].slice(
-      :patient_id, :administration_type, :evaluation_type, :administration_date, :source, :embletta_unit_number, :expected_receipt_date, :receipt_date, :storage_date, :subject_notified, :reimbursement_form_date, :scored_date, :ahi, :eligibility, :exclusion, :status, :comments
-    )
-  end
+    def generate_csv(evaluation_scope)
+      @csv_string = CSV.generate do |csv|
+        csv << ["Patient ID", "Subject Code", "Administration Type", "Evaluation Type", "Source", "Administration Date", "Receipt Date", "Scored Date", "AHI", "Eligibility", "Exclusion", "Exclusion Code", "Status", "Comments"]
+        evaluation_scope.each do |evaluation|
+          csv << [
+            evaluation.patient.id,
+            evaluation.patient.subject_code,
+            evaluation.administration_type_name,
+            evaluation.evaluation_type_name,
+            evaluation.source,
+            evaluation.administration_date.strftime("%Y-%m-%d"),
+            evaluation.receipt_date ? evaluation.receipt_date.strftime("%Y-%m-%d") : '',
+            evaluation.scored_date ? evaluation.scored_date.strftime("%Y-%m-%d") : '',
+            evaluation.ahi,
+            evaluation.eligibility,
+            evaluation.exclusion_name,
+            evaluation.exclusion,
+            evaluation.status,
+            evaluation.comments
+          ]
+        end
+      end
+      send_data @csv_string, type: 'text/csv; charset=iso-8859-1; header=present',
+                            disposition: "attachment; filename=\"Evaluations #{Time.now.strftime("%Y.%m.%d %Ih%M %p")}.csv\""
+    end
 
 end
