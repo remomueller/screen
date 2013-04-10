a790623d955d02441175b84a15a74854105c3f4b
Remo Mueller <remosm@gmail.com>
2013-04-10 12:44:52 -0400

    Updated Clinics with Rails 4 strong_parameters

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,93 +1,88 @@
 class ClinicsController < ApplicationController
   before_action :authenticate_user!
   before_action :check_screener
+  before_action :set_clinic, only: [ :show, :edit, :update, :destroy ]
+  before_action :redirect_without_clinic, only: [ :show, :edit, :update, :destroy ]
 
+  # GET /clinics
+  # GET /clinics.json
   def index
-    # current_user.update_column :clinics_per_page, params[:clinics_per_page].to_i if params[:clinics_per_page].to_i >= 10 and params[:clinics_per_page].to_i <= 200
-    clinic_scope = Clinic.current # current_user.all_viewable_clinics
-    @search_terms = params[:search].to_s.gsub(/[^0-9a-zA-Z]/, ' ').split(' ')
-    @search_terms.each{|search_term| clinic_scope = clinic_scope.search(search_term) }
-
     @order = scrub_order(Clinic, params[:order], 'clinics.name')
-    clinic_scope = clinic_scope.order(@order)
-
-    @clinic_count = clinic_scope.count
-    @clinics = clinic_scope.page(params[:page]).per(40) # (current_user.clinics_per_page)
+    @clinics = Clinic.current.search(params[:search]).order(@order).page(params[:page]).per(40)
   end
 
   # GET /clinics/1
   # GET /clinics/1.json
   def show
-    @clinic = Clinic.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @clinic }
-    end
   end
 
   # GET /clinics/new
-  # GET /clinics/new.json
   def new
     @clinic = Clinic.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @clinic }
-    end
   end
 
   # GET /clinics/1/edit
   def edit
-    @clinic = Clinic.find(params[:id])
   end
 
   # POST /clinics
   # POST /clinics.json
   def create
-    @clinic = current_user.clinics.new(post_params)
+    @clinic = current_user.clinics.new(clinic_params)
 
     respond_to do |format|
       if @clinic.save
         format.html { redirect_to @clinic, notice: 'Clinic was successfully created.' }
-        format.json { render json: @clinic, status: :created, location: @clinic }
+        format.json { render action: 'show', status: :created, location: @clinic }
       else
-        format.html { render action: "new" }
+        format.html { render action: 'new' }
         format.json { render json: @clinic.errors, status: :unprocessable_entity }
       end
     end
   end
 
+  # PUT /clinics/1
+  # PUT /clinics/1.json
   def update
-    @clinic = Clinic.find(params[:id])
-
-    if @clinic.update_attributes(post_params)
-      notice = 'Clinic was successfully updated.'
-      if params[:from] == 'prescreens'
-        redirect_to prescreens_path, notice: notice
+    respond_to do |format|
+      if @clinic.update(clinic_params)
+        format.html do
+          url = (params[:from] == 'prescreens' ? prescreens_path : @clinic)
+          redirect_to url, notice: 'Clinic was successfully updated.'
+        end
+        format.json { head :no_content }
       else
-        redirect_to @clinic, notice: notice
+        format.html { render action: 'edit' }
+        format.json { render json: @clinic.errors, status: :unprocessable_entity }
       end
-    else
-      render action: "edit"
     end
   end
 
+  # DELETE /clinics/1
+  # DELETE /clinics/1.json
   def destroy
-    @clinic = Clinic.find(params[:id])
     @clinic.destroy
 
-    redirect_to clinics_path, notice: 'Clinic was successfully deleted.'
+    respond_to do |format|
+      format.html { redirect_to clinics_path, notice: 'Clinic was successfully deleted.' }
+      format.json { head :no_content }
+    end
   end
 
   private
 
-  def post_params
-    params[:clinic] ||= {}
+    def set_clinic
+      @clinic = Clinic.find_by_id(params[:id])
+    end
 
-    params[:clinic].slice(
-      :name, :status
-    )
-  end
+    def redirect_without_clinic
+      empty_response_or_root_path(clinics_path) unless @clinic
+    end
+
+    def clinic_params
+      params.require(:clinic).permit(
+        :name, :status
+      )
+    end
 
 end
